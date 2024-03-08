8658a88ed1d5d5a7b2dad7d733bf4ff86ff50cdd
Chibuike Mba <chibuike.code@gmail.com>
2024-03-08 20:15:31 +0100

    Update App.jsx, main.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,49 @@
+import Links from "./components/Links";
+import Sidebar from "./components/Sidebar";
+import Hero from "./components/Hero";
+import { RiMenu3Fill } from "react-icons/ri";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { useGlobalContextApp } from "./ContextApp";
+
 const App = () => {
-    return <div>App</div>;
+    const { isOpen, setIsOpen } = useGlobalContextApp();
+
+    return (
+        <>
+            <header>
+                <div
+                    className={`top-header-div ${
+                        isOpen ? "" : "hide-top-header-div"
+                    }`}
+                >
+                    <h1>
+                        Innovi<span className="title-span">Craft</span>
+                    </h1>
+                    <button
+                        type="button"
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="menu-button"
+                    >
+                        <RiMenu3Fill />
+                    </button>
+                </div>
+
+                <nav className={`nav ${isOpen ? "hide-nav" : ""}`}>
+                    <div className="close-button">
+                        <button
+                            type="button"
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            <MdOutlineRestaurantMenu />
+                        </button>
+                    </div>
+                    <Sidebar />
+                </nav>
+                <Links />
+            </header>
+            <Hero />
+        </>
+    );
 };
+
 export default App;
